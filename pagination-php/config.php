e937bac88bee47a6661c6ef591e7b87e63eaabd4
Pallavi <pallavi.jakanachary@sjsu.edu>
2018-03-10 12:14:50 -0800

    Update config.php

--- pagination-php/config.php
+++ pagination-php/config.php
@@ -1,12 +1,18 @@
 <?php
-$user = "root";
-$pass = "password";
-$conn       = new mysqli( '127.0.0.1', 'root', 'password', 'movieDB' );
+//$user = "root";
+//$pass = "password";
+
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "ba0dd49e70befd";
+$password = "e8e0885d";
+$dbname = "heroku_54c3b520208a1ef";
+
+$conn       = new mysqli( 'us-cdbr-iron-east-05.cleardb.net', 'ba0dd49e70befd', 'e8e0885d', 'heroku_54c3b520208a1ef' );
 
 $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 25;
 $page       = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
 $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
-$query = "SELECT * FROM movies ";
+$query = "select * from movies";
 
 require_once 'paginator.class.php';
 $paginator  = new Paginator($conn, $query);
